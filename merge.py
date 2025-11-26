f019a9dfc76002ab65a1493361b1e3767bb93441
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2025-11-27 03:10:48 +0800

    q2.1 merge

---
+++ merge.py
@@ -0,0 +1,13 @@
+def mergeTwoLists(l1, l2):
+  if l1 is None:
+    return l2
+  if l2 is None:
+    return l1
+  
+  if l1.val <= l2.val:
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  else:
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
+  
